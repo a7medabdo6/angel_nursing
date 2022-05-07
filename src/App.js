8ccd4684d8d6710f321c7d1addea68d571abfa1a
a7med.abdo6 <a7med.abdo6@gmail.com>
2022-05-07 06:53:17 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+ import Login from "./pages/Login"
+ import Register from "./pages/Register"
+import Thank from './pages/Thank';
+import Reset from "./pages/Reset"
+import ContactUs from "./pages/ContactUs"
+import Dashboard from "./pages/Dashboard"
+import useMediaQuery from '@mui/material/useMediaQuery';
+import Privacy from './pages/Privacy';
 
 function App() {
+  const matches = useMediaQuery('(max-width:970px)');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{marginBlock:matches?"80px":"0px"}}>
+      <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/thank" element={<Thank />} />
+      <Route path="/reset-password" element={<Reset />} />
+      <Route path="/contact" element={<ContactUs />} />
+      <Route path="/privacy" element={<Privacy />} />
+
+      <Route path="/dashboard" element={<Dashboard />} />
+
+    </Routes>
     </div>
   );
 }
